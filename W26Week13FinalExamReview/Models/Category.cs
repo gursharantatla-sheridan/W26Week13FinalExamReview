0a459f21b9823b0b05ccafc45626f571b7961509
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-04-08 13:21:03 -0400

    model

---
+++ W26Week13FinalExamReview/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace W26Week13FinalExamReview.Models
+{
+    public class Category
+    {
+        // scalar properties
+        public int CategoryId { get; set; }
+        public string? CategoryName { get; set; }
+
+        // navigation property
+        public ICollection<Product>? Products { get; set; }
+    }
+}
---
+++ W26Week13FinalExamReview/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace W26Week13FinalExamReview.Models
+{
+    public class Product
+    {
+        // scalar properties
+        public int ProductId { get; set; }  // primary key
+        public string? ProductName { get; set; }
+        public double? Price { get; set; }
+        public bool? IsDiscontinued { get; set; }
+        public int? CategoryId { get; set; }  // foreign key
+
+        // navigation property
+        public Category? Category { get; set; }
+    }
+}
